7540d23df3ef2073e283cc4908a517903a6325df
samuel-cavalcanti <scavalcanti111@gmail.com>
2022-05-09 19:21:52 -0300

    movido operações nos nós para o arquivo node.hpp

--- question_3/question_3.cpp
+++ question_3/question_3.cpp
@@ -6,45 +6,6 @@
 #include <numeric>
 bool DEBUG = false;
 
-int compute_data(std::vector<int> *data)
-{
-    auto total = 0;
-    auto size = data->size();
-    for (auto i = 0; i < size; i++)
-    {
-        total += data->at(i);
-    }
-
-    return total;
-}
-int compute_node(Node &node)
-{
-
-    int result_data = node.data == nullptr ? 0 : compute_data(node.data);
-
-    for (auto neighborhood : node.neighborhoods)
-        result_data += compute_node(*neighborhood);
-
-    return result_data;
-}
-
-Node *create_new_tree(std::vector<Node *> nodes)
-{
-
-    auto size = nodes.size();
-
-    for (auto i = 0; i < size; i += 2)
-    {
-        auto left = nodes[i];
-        auto right = nodes[i + 1];
-        left->neighborhoods.push_back(right);
-
-        if (i != 0)
-            nodes[0]->neighborhoods.push_back(left);
-    }
-
-    return nodes[0];
-}
 
 int main(int argc, char const *argv[])
 {
