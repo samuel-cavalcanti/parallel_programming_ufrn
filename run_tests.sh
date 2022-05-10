692f6d4f06824802addce714fac7683b4eadbf25
samuel-cavalcanti <scavalcanti111@gmail.com>
2022-05-10 15:49:58 -0300

    adicionado a questão 5 no caso de testes

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,6 @@
 gcc question_1/question_1.c -o 1.out; ./1.out
 python question_2/question_2.py
 g++ question_3/question_3.cpp -o 3.out; ./3.out 
-g++ question_4/question_4.cpp -o 4.out; ./4.out 
+g++ question_4/question_4.cpp -o 4.out; ./4.out
+g++ question_5/question_5.cpp -o 5.out; ./5.out
 rm *.out
