da9e02d61b3ca2a2023f8f522e589aa5c9638f29
samuel-cavalcanti <scavalcanti111@gmail.com>
2022-05-28 19:06:19 -0300

    removido assets do git

--- build_pdf.sh
+++ build_pdf.sh
@@ -24,3 +24,7 @@ for chapter_dir in chapter_1 chapter_2; do
 
     
 done
+
+## código para executar com uma linha
+## lembre-se de colocar as fotos na raiz do repo
+##pandoc chapter_1/question_1/Readme.md  chapter_1/question_2/Readme.md chapter_1/question_3/Readme.md chapter_1/question_4/Readme.md chapter_1/question_5/Readme.md chapter_1/question_6/Readme.md chapter_1/question_9/Readme.md chapter_2/question_1/Readme.md   chapter_2/question_2/Readme.md chapter_2/question_3/Readme.md chapter_2/question_5/Readme.md chapter_2/question_7/Readme.md chapter_2/question_10/Readme.md chapter_2/question_15/Readme.md chapter_2/question_16/Readme.md chapter_2/question_17/Readme.md chapter_2/question_19/Readme.md chapter_2/question_20/Readme.md chapter_2/question_21/Readme.md chapter_2/question_24/Readme.md -o  output.pdf --pdf-engine=xelatex
