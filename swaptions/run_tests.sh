de64dd58b9cf1f4e22a1b9460d7ec8d0eca275fe
samuel-cavalcanti <scavalcanti111@gmail.com>
2022-07-22 00:29:08 -0300

    adicionado configurações para  binários de testes

--- swaptions/run_tests.sh
+++ swaptions/run_tests.sh
@@ -9,11 +9,7 @@ cmake ".."
 make -j8
 cd ..
 
-NTHREADS=4;
-
-simmedium="-ns 32 -sm 20000 -nt ${NTHREADS}";
-simsmall="-ns 16 -sm 10000 -nt ${NTHREADS}";
-
-./build/swaptions-pthreads $simsmall;
-
-./build/swaptions-openmp $simsmall;   
+echo "running Pthreads tests"
+./build/swaptions-pthreads-test $simsmall;
+echo "running OpenMP tests"
+./build/swaptions-openmp-test $simsmall;   
